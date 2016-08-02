6155506479d544f6029e37d105db4ab616e47902
Yehonathan Sharvit <viebel@gmail.com>
2016-08-02 14:46:40 +0300

    Update macros.clj

--- src/gadjett/macros.clj
+++ src/gadjett/macros.clj
@@ -1,17 +1,17 @@
 (ns gadjett.macros)
 
 
-#_(defn symbol-several
+(defn symbol-several
   "returns a symbol with the concatenation of the str values of the args"
   [& x]
   (symbol (apply str x)))
 
-#_(defmacro disp [& forms]
+(defmacro disp [& forms]
   (cons
     `symbol-several
     (for [form forms]
       `(str '~form " =>> " ~form "\n"))))
     
-(defmacro disp [& forms]
+#_(defmacro disp [& forms]
   (cons `str (for [form forms]
                `(str (pr-str '~form) " => " (pr-str ~form)))))
